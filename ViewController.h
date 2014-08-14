1126b02b44ff62880ec3a11cb3da88e616891d5d
hrak4 <hiro.apple2014@gmail.comgit config --global user.name hrak4>
2014-08-14 11:35:18 +0800

    test
    This is test

---
+++ ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SampleAlertView
+//
+//  Created by OkadaHiroaki on 2014/07/11.
+//  Copyright (c) 2014年 HiroakiOkada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  SampleAlertView
+//
+//  Created by OkadaHiroaki on 2014/07/11.
+//  Copyright (c) 2014年 HiroakiOkada. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+        initWithTitle:@"今日アクティビティ行きますか？"
+        message:@"行きますよね？"
+        delegate:self
+        cancelButtonTitle:@"Cancel"
+        otherButtonTitles:@"OK", nil];
+    
+    [alert show];
+}
+
+
+-(void)alertView:(UIAlertView *)alertView
+    clickedButtonAtIndex:(NSInteger)
+    buttonIndex
+{
+    if (buttonIndex == 1) {
+        NSLog(@"OK");
+        self.myLabel.text = @"アクティビティ楽しんで！";
+    }else{
+        NSLog(@"Cancel");
+        self.myLabel.text = @"次回はアクティビティ行きましょう！";
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
